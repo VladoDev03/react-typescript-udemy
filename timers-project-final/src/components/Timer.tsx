39767e9ebac3ffeddbee929152e9e85d9d8ecc4a
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-01 22:13:21 +0200

    Fixed a side effect in Timer component.

--- timers-project-final/src/components/Timer.tsx
+++ timers-project-final/src/components/Timer.tsx
@@ -1,11 +1,27 @@
 import Container from './UI/Container.tsx';
 import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useEffect, useState } from 'react';
+
+const MILLISECONDS = 1000;
+const INTERVAL = 50;
+const TO_FIXED = 2;
 
 export default function Timer({ name, duration }: TimerProps) {
+	const [remainingTime, setRemainingTime] = useState(duration * MILLISECONDS);
+
+	useEffect(() => {
+		setInterval(() => {
+			setRemainingTime(prevTime => prevTime - INTERVAL);
+		}, INTERVAL);
+	}, []);
+
+	const formattedRemainingTime = (remainingTime / MILLISECONDS).toFixed(TO_FIXED);
+
 	return (
 		<Container as="article">
 			<h2>{name}</h2>
-			<p>{duration}</p>
+			<p><progress max={duration * MILLISECONDS} value={remainingTime} /></p>
+			<p>{formattedRemainingTime}</p>
 		</Container>
 	);
 }
