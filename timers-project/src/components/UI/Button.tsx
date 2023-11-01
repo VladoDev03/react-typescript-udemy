7901c775ed0437e4dbde75d609baef0eb34e4fa1
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-01 21:37:32 +0200

    Finished context module.

--- timers-project/src/components/UI/Button.tsx
+++ timers-project/src/components/UI/Button.tsx
@@ -1,21 +1,21 @@
 import { type ComponentPropsWithoutRef } from 'react';
 
 type ButtonProps = ComponentPropsWithoutRef<'button'> & {
-  href?: never;
+    href?: never;
 };
 
 type AnchorProps = ComponentPropsWithoutRef<'a'> & {
-  href?: string;
+    href?: string;
 };
 
 function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
-  return 'href' in props;
+    return 'href' in props;
 }
 
 export default function Button(props: ButtonProps | AnchorProps) {
-  if (isAnchorProps(props)) {
-    return <a className="button" {...props}></a>;
-  }
+    if (isAnchorProps(props)) {
+        return <a className="button" {...props}></a>;
+    }
 
-  return <button className="button" {...props}></button>;
+    return <button className="button" {...props}></button>;
 }
--- timers-project/src/components/UI/Container.tsx
+++ timers-project/src/components/UI/Container.tsx
@@ -1,23 +1,23 @@
 import {
-  type ReactNode,
-  type ElementType,
-  type ComponentPropsWithoutRef,
+	type ReactNode,
+	type ElementType,
+	type ComponentPropsWithoutRef,
 } from 'react';
 
 type ContainerProps<T extends ElementType> = {
-  as?: T;
-  children: ReactNode;
+	as?: T;
+	children: ReactNode;
 } & ComponentPropsWithoutRef<T>;
 
 export default function Container<C extends ElementType>({
-  as,
-  children,
-  ...props
+	as,
+	children,
+	...props
 }: ContainerProps<C>) {
-  const Component = as || 'div';
-  return (
-    <Component {...props}>
-      {children}
-    </Component>
-  );
+	const Component = as || 'div';
+	return (
+		<Component {...props}>
+			{children}
+		</Component>
+	);
 }
