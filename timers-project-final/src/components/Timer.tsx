748475d7c9c42611678bbde0f10d9cf9f6a65c54
VladoDev03 <vladsto101@gmail.com>
2023-11-02 11:52:26 +0200

    Finished timers project.

--- timers-project-final/src/components/Timer.tsx
+++ timers-project-final/src/components/Timer.tsx
@@ -1,19 +1,43 @@
 import Container from './UI/Container.tsx';
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
-import { useEffect, useState } from 'react';
+import { useTimersContext, type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useEffect, useRef, useState } from 'react';
 
 const MILLISECONDS = 1000;
 const INTERVAL = 50;
 const TO_FIXED = 2;
 
 export default function Timer({ name, duration }: TimerProps) {
+	const interval = useRef<number | null>(null);
 	const [remainingTime, setRemainingTime] = useState(duration * MILLISECONDS);
+	const {isRunning} = useTimersContext();
+
+	if (remainingTime <= 0 && interval.current)  {
+		clearInterval(interval.current);
+	}
 
 	useEffect(() => {
-		setInterval(() => {
-			setRemainingTime(prevTime => prevTime - INTERVAL);
-		}, INTERVAL);
-	}, []);
+		let timer: number;
+
+		if (isRunning) {
+			timer = setInterval(() => {
+				setRemainingTime(prevTime => {
+					if (prevTime <= 0) {
+						return prevTime;
+					}
+
+					return prevTime - INTERVAL;
+				});
+			}, INTERVAL);
+
+			interval.current = timer;
+		} else if (interval.current) {
+			clearInterval(interval.current);
+		}
+
+		return () => {
+			clearInterval(timer);
+		}
+	}, [isRunning]);
 
 	const formattedRemainingTime = (remainingTime / MILLISECONDS).toFixed(TO_FIXED);
 
